38dc25ce1f36d923b6f954b5d750a542448b88b8
Vasudev Kishan <vasudevkishan@gmail.com>
2022-11-07 21:01:54 +0530

    first commit

--- src/SearchItem.js
+++ src/SearchItem.js
@@ -3,9 +3,11 @@ const SearchItem = ({ search, setSearch }) => {
     return (
     <form className='searchForm' onSubmit={(e)=>e.preventDefault()}>
         <label htmlFor='search'>Search</label>
+        
         <input
             id='search'
             type='text'
+            // eslint-disable-next-line
             role='searchBox'
             placeholder='Search Items'
             value={search}
